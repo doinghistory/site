e7756c03047b9e86dc832070bd2389ef1001cea7
doinghistory <kosuke.sugisaki@daltontokyo.ed.jp>
2026-09-22 21:59:51 +0900

    Add files via upload

---
+++ site-config.js
@@ -0,0 +1,10 @@
+window.HISTORY_SITE_CONFIG = {
+  // 「学ぶ」「問う」「観る」で共通利用する公開CSV
+  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGfy6nXuXVIFiazfphIAJT7by7hcBLolbP6DHFrIxqrbyB8ucFAlzIRUGuK3BERm9p1IFCi07cvgPu/pub?gid=1464622297&single=true&output=csv",
+
+  // 実URLが分かり次第、ここだけ書き換えれば「問う」の2アプリに反映されます。
+  APPS: {
+    yamakawa: "https://doinghistory.github.io/nihonshi-q-a/",
+    jidaiToNagare: "https://doinghistory.github.io/nihonshi-exercise/"
+  }
+};
