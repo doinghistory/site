0b14c9626c1f6179096a92a31fdfb32f517d01b9
doinghistory <kosuke.sugisaki@daltontokyo.ed.jp>
2026-09-23 13:14:16 +0900

    Use new public materials CSV

--- site-config.js
+++ site-config.js
@@ -1,6 +1,6 @@
 window.HISTORY_SITE_CONFIG = {
   // 「学ぶ」「問う」「観る」で共通利用する公開CSV
-  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGfy6nXuXVIFiazfphIAJT7by7hcBLolbP6DHFrIxqrbyB8ucFAlzIRUGuK3BERm9p1IFCi07cvgPu/pub?gid=1464622297&single=true&output=csv",
+  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRqBole4i2mxRQPC6VsDdrRVXZQmLFDVmBoE0y0TkEDcpCd_J71F0IZol81MEX4qzN6EkbcI8RIIoeu/pub?gid=1696967938&single=true&output=csv",
 
   // 実URLが分かり次第、ここだけ書き換えれば「問う」の2アプリに反映されます。
   APPS: {
